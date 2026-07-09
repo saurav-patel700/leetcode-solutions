bd5c11fd0ddcc42cb08c3ae15feda3b84ba49d0a
saurav kumar <saurav392003@gmail.com>
2026-07-09 12:49:05 +0530

    Time: 2 ms (65.85%), Space: 17.7 MB (97.18%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -9,12 +9,17 @@ public:
             nums[i]+=nums[i-1];
         }
         for(int i=0;i<m;i++){
-            int len =0;
-            for(int j=0;j<n;j++){
-                if(nums[j]>queries[i]) break;
-                len++;
+            int maxlen =0;
+            int lo=0,hi=n-1;
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(nums[mid]>queries[i]) hi=mid-1;
+                else{
+                    maxlen=mid+1;
+                    lo=mid+1;
+                }
             }
-            ans[i]=len;
+            ans[i]=maxlen;
         }
         return ans;
     }
