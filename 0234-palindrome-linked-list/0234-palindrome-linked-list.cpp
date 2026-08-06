043f7d9dfab8a3f9d1d0a55b2d45ca4de3569e24
saurav kumar <saurav392003@gmail.com>
2026-08-06 11:58:11 +0530

    Time: 0 ms (100%), Space: 118 MB (82.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,5 +1,27 @@
-class Solution {
-public:
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* c=new ListNode(10);
+//         ListNode* temp=head;
+//         ListNode* tempC=c;
+//         while(temp){
+//             ListNode* node = new ListNode(temp->val);
+//             tempC->next=node;
+//             temp=temp->next;
+//             tempC=tempC->next;
+//         }
+//         c=c->next;
+//         c=reverseList(c);
+//         ListNode* a=head;
+//         ListNode* b=c;
+//         while(a){
+//             if(a->val!=b->val) return false;
+//             a=a->next;
+//             b=b->next;
+//         }
+//         return true;
+//     }
+// };
     ListNode* reverseList(ListNode* head) {
         ListNode *prev=NULL, *curr=head,*Next=head;
         while(curr){
@@ -10,25 +32,23 @@ public:
         }
         return prev;
     }
+class Solution {
+public:
     bool isPalindrome(ListNode* head) {
-        ListNode* c=new ListNode(10);
-        ListNode* temp=head;
-        ListNode* tempC=c;
-        while(temp){
-            ListNode* node = new ListNode(temp->val);
-            tempC->next=node;
-            temp=temp->next;
-            tempC=tempC->next;
-        }
-        c=c->next;
-        c=reverseList(c);
-        ListNode* a=head;
-        ListNode* b=c;
-        while(a){
-            if(a->val!=b->val) return false;
-            a=a->next;
-            b=b->next;
-        }
-        return true;
+       ListNode* slow=head; 
+       ListNode* fast=head;
+       while(fast->next && fast->next->next){
+        slow = slow->next;
+        fast= fast->next->next;
+       }
+       ListNode* newHead=reverseList(slow->next); 
+       ListNode* a=head; 
+       ListNode* b=newHead;
+       while(b){
+        if(a->val!=b->val) return false;
+        a=a->next;
+        b=b->next;
+       }
+       return true;
     }
 };
