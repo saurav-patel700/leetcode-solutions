3aefacbf39910fac76d3aa49869d762fed3cbd5e
saurav kumar <saurav392003@gmail.com>
2026-08-07 16:30:27 +0530

    Time: 4 ms (58.25%), Space: 124.6 MB (62.53%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -7,31 +7,18 @@ public:
         ListNode* a=head;
         ListNode* b=head->next;
         ListNode* c=head->next->next;
+        if(c==NULL) return {-1,-1};
+        int mind=INT_MAX;
+        int f=-1;
+        int s=-1;
         while(c){
             if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val){
                 if(fidx==-1) fidx=idx;
                 else sidx=idx;
-            }
-            a=a->next;
-            b=b->next;
-            c=c->next;
-            idx++;
-        }
-        if(sidx==-1) return {-1,-1};
-        int maxd=sidx-fidx;
-        int mind=INT_MAX;
-        fidx=-1;
-        sidx=-1;
-        idx=1;
-        a=head;
-        b=head->next;
-        c=head->next->next;
-        while(c){
-            if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val){
-                fidx=sidx;
-                sidx=idx;
-                if(fidx!=-1){
-                    int d=sidx-fidx;;
+                f=s;
+                s=idx;
+                if(f!=-1){
+                    int d=s-f;
                     mind=min(mind,d);
                 }
             }
@@ -40,6 +27,8 @@ public:
             c=c->next;
             idx++;
         }
+        if(sidx==-1) return {-1,-1};
+        int maxd=sidx-fidx;
         return {mind,maxd};
     }
 };
