253598c230df6cfba701b06233187a6b400d9ace
saurav kumar <saurav392003@gmail.com>
2026-08-13 22:43:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2!=0) return false;
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else {
+                if(st.size()==0) return false;
+                
+                if(s[i]==')' && st.top()!='(') return false;
+                if(s[i]=='}' && st.top()!='{') return false;
+                if(s[i]==']' && st.top()!='[') return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
