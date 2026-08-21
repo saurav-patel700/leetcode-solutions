f6515778e4f07da530e65310e41b2a3800bbc3aa
saurav kumar <saurav392003@gmail.com>
2026-08-21 21:20:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (58.18%) - LeetHub

---
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+        int digitsum(int x){
+            int sum=0;
+            while(x>0){
+                sum+=x%10;
+                x/=10;
+            }
+            return sum;
+        }
+    int differenceOfSum(vector<int>& nums) {
+        int n=nums.size();
+        int esum=0;
+        int dsum=0;
+        for(int i=0;i<n;i++){
+            esum+=nums[i];
+            if(nums[i]<=9){
+                dsum+=nums[i];
+            }
+            else {
+                    dsum+=digitsum(nums[i]);
+                }
+        }
+        return abs(esum-dsum);
+    }
+};
