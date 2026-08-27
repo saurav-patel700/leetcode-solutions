407098109b6230cbd9a23620dd57f2b24085978b
saurav kumar <saurav392003@gmail.com>
2026-08-27 18:31:33 +0530

    Time: 27 ms (42.53%), Space: 139.1 MB (73.86%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,47 @@
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+//         if(k==1) return arr;
+//         int n=arr.size();
+//         vector<int>ngi(n);
+//         stack<int> st;
+//         ngi[n-1]=n;
+//         st.push(n-1);
+//         for(int i=n-2;i>=0;i--){
+//             while(st.size()>0 && arr[st.top()]<=arr[i]){
+//                 st.pop();
+//             }
+//             if(st.size()==0) ngi[i]=n;
+//             else ngi[i]=st.top();
+//             st.push(i);
+//         }
+//         vector<int>ans;
+//         int j=0;
+//         for(int i=0;i<n-k+1;i++){
+//             if(j<i) j=i;
+//             while(ngi[j]<i+k){
+//                 j=ngi[j];
+//             }
+//             ans.push_back(arr[j]);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        if(k==1) return arr;
-        int n=arr.size();
-        vector<int>ngi(n);
-        stack<int> st;
-        ngi[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(st.size()>0 && arr[st.top()]<=arr[i]){
-                st.pop();
-            }
-            if(st.size()==0) ngi[i]=n;
-            else ngi[i]=st.top();
-            st.push(i);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        deque<int> dq;
+        vector<int> ans;
+        if(k==1) return nums;
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[i]>nums[dq.back()]) 
+            dq.pop_back();
+            dq.push_back(i);
+            int j=i-k+1;
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
-        vector<int>ans;
-        int j=0;
-        for(int i=0;i<n-k+1;i++){
-            if(j<i) j=i;
-            while(ngi[j]<i+k){
-                j=ngi[j];
-            }
-            ans.push_back(arr[j]);
-        }
-        return ans;
+        return ans; 
     }
 };
