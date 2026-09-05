e6b0a97aa5c2093368062b801e87dd87d474019e
saurav kumar <saurav392003@gmail.com>
2026-09-05 17:02:04 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.92%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,33 +1,59 @@
+// class Solution {
+// public:
+// void nthlevel(TreeNode* root,int curr,int level,vector<int>& v){
+//     if(root==NULL) return;
+//     if(curr==level){ 
+//         v.push_back(root->val);
+//         return;
+//     }
+//     nthlevel(root->left,curr+1,level,v);
+//     nthlevel(root->right,curr+1,level,v);
+// }
+// int levels(TreeNode* root){
+//     if(root==NULL) return 0;
+//     int lmax=levels(root->left);
+//     int rmax=levels(root->right);
+//     return 1 + max(lmax,rmax);
+// }
+// void lorder(TreeNode* root,vector<vector<int>>& ans){
+//     int n=levels(root);
+//     for(int i=1;i<=n;i++){
+//         vector<int> v;
+//         nthlevel(root,1,i,v);
+//         ans.push_back(v);
+//         cout<<endl; 
+//     }
+// }
+
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> ans;
+//         lorder(root,ans);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-void nthlevel(TreeNode* root,int curr,int level,vector<int>& v){
-    if(root==NULL) return;
-    if(curr==level){ 
-        v.push_back(root->val);
-        return;
-    }
-    nthlevel(root->left,curr+1,level,v);
-    nthlevel(root->right,curr+1,level,v);
-}
 int levels(TreeNode* root){
     if(root==NULL) return 0;
     int lmax=levels(root->left);
     int rmax=levels(root->right);
     return 1 + max(lmax,rmax);
 }
-void lorder(TreeNode* root,vector<vector<int>>& ans){
-    int n=levels(root);
-    for(int i=1;i<=n;i++){
-        vector<int> v;
-        nthlevel(root,1,i,v);
-        ans.push_back(v);
-        cout<<endl; 
-    }
+void lorder(TreeNode* root,vector<vector<int>>& ans,int level){
+    if(root==NULL) return;
+    ans[level].push_back(root->val);
+    lorder(root->left,ans,level+1);
+    lorder(root->right,ans,level+1);
 }
 
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n=levels(root);
         vector<vector<int>> ans;
-        lorder(root,ans);
+        for(int i=1;i<=n;i++){
+            vector<int> v;
+            ans.push_back(v);
+        }
+        lorder(root,ans,0);
         return ans;
     }
 };
